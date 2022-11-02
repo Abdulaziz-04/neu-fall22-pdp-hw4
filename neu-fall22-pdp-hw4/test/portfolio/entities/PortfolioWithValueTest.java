390383b438ec91a8ee33cc6c25374042ea9c0d03
pa <pa@myo.in.th>
2022-11-02 01:11:46 -0400

    PortfolioWithValueTest

--- neu-fall22-pdp-hw4/test/portfolio/entities/PortfolioWithValueTest.java
+++ neu-fall22-pdp-hw4/test/portfolio/entities/PortfolioWithValueTest.java
@@ -3,6 +3,7 @@ package portfolio.entities;
 import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,24 +16,21 @@ import org.junit.Test;
  * This is a test class to test PortfolioWithValue class.
  */
 public class PortfolioWithValueTest {
-
-  private Map<String, Integer> stocks = new HashMap<>();
-  private Map<Map<String, Integer>, Double> stocksWithValue =new HashMap<>();
+  private List<PortfolioEntryWithValue> stocksWithValue = new ArrayList<>();
   LocalDate date;
   private PortfolioWithValue portfolioWithValue;
 
   @Before
   public void setup() {
     date = LocalDate.parse("2022-10-10");
-    stocksWithValue.put(Map.of("AAA",100),100.1);
-    stocksWithValue.put(Map.of("AA",200),200.2);
-    portfolioWithValue = new PortfolioWithValue(date, (List<PortfolioEntryWithValue>) stocksWithValue,50050);
+    stocksWithValue.add(new PortfolioEntryWithValue(new PortfolioEntry("AAA", 100), 100.1));
+    stocksWithValue.add(new PortfolioEntryWithValue(new PortfolioEntry("AA", 200), 200.2));
+    portfolioWithValue = new PortfolioWithValue(date, stocksWithValue, 50050);
   }
 
   @Test
   public void test() {
-    assertEquals("2022-10-10",portfolioWithValue.getDate());
-    assertEquals(50050,portfolioWithValue.getTotalValue());
-
+    assertEquals(LocalDate.parse("2022-10-10"), portfolioWithValue.getDate());
+    assertEquals(50050, portfolioWithValue.getTotalValue());
   }
 }
