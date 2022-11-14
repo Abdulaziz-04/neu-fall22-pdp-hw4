898c8704ad4155efbb74a59cfa9ffedd29c20a48
Vicky-Zhao97 <zhao.wei3@northeastern.edu>
2022-11-14 14:41:18 -0500

    portfolio performance

---
+++ neu-fall22-pdp-hw4/src/portfolio/controllers/impl/PerformancePageController.java
@@ -0,0 +1,422 @@
+package portfolio.controllers.impl;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import portfolio.controllers.PageController;
+import portfolio.controllers.PageControllerFactory;
+import portfolio.models.portfolio.Portfolio;
+import portfolio.models.portfolio.PortfolioService;
+import portfolio.views.View;
+import portfolio.views.ViewFactory;
+
+public class PerformancePageController implements PageController  {
+  private final PortfolioService portfolioService;
+  private final PageControllerFactory controllerFactory;
+  private final ViewFactory viewFactory;
+  private String errorMessage;
+
+  private Map<LocalDate, Double> map;
+
+  private String portfolioName;
+
+  private LocalDate startDate;
+  private LocalDate endDate;
+
+  private List<String>  listStar = new ArrayList<>();
+  private List<String> list = new ArrayList<>();
+  private String scale;
+
+
+  public PerformancePageController(PortfolioService portfolioService,
+                            PageControllerFactory controllerFactory, ViewFactory viewFactory) {
+    this.portfolioService = portfolioService;
+    this.controllerFactory = controllerFactory;
+    this.viewFactory = viewFactory;
+  }
+
+  private void updateOutput(LocalDate startDate, LocalDate endDate) {
+    List<Double> listAmount = new ArrayList<>();
+    long dayCount = ChronoUnit.DAYS.between(startDate,endDate) + 1;
+    long weekCount = ChronoUnit.WEEKS.between(startDate,endDate) + 1;
+    long monthCount = ChronoUnit.MONTHS.between(startDate,endDate) + 1 ;
+    long yearCount = ChronoUnit.YEARS.between(startDate,endDate) + 1;
+    LocalDate currentDate =  startDate;
+    if(dayCount <= 30) {
+      //output the list
+      while (!currentDate.isAfter(endDate)) {
+        if(map.containsKey(currentDate)) {
+          list.add(currentDate + ":");
+          listAmount.add(map.get(currentDate));
+          currentDate = currentDate.plusDays(1);
+        } else {
+          LocalDate currentGet = currentDate.minusDays(1);
+          while(!map.containsKey(currentGet)) {
+            currentGet = currentGet.minusDays(1);
+          }
+          list.add(currentDate + ":");
+          listAmount.add(map.get(currentGet));
+        }
+      }
+    } else if (weekCount <=30) {
+      //split it to weeks
+      // find the last working day of this week
+      while(!currentDate.isAfter(endDate)) {
+        LocalDate currentWeekEnd = currentDate.with(ChronoField.DAY_OF_WEEK, 7);
+        LocalDate currentWeekGet = currentDate.with(ChronoField.DAY_OF_WEEK, 5);
+        if (currentWeekEnd.isAfter(endDate)) {
+          currentWeekEnd = endDate;
+          currentWeekGet = endDate;
+        }
+        while(!map.containsKey(currentWeekGet)) {
+          currentWeekGet = currentWeekGet.minusDays(1);
+        }
+        list.add(currentDate + "to " + currentWeekEnd + ":" /*+ map.get(currentWeekGet)*/);
+        listAmount.add(map.get(currentWeekGet));
+        currentDate = currentWeekEnd.plusDays(1);
+      }
+    } else if (monthCount <= 30) {
+      //split it to month
+      // find the last working day of this month
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentMonthEnd = currentDate.withDayOfMonth(currentDate.lengthOfMonth());
+        LocalDate currentMonthGet = currentMonthEnd;
+        if(currentMonthEnd.isAfter(endDate)) {
+          currentMonthEnd = endDate;
+          currentMonthGet = endDate;
+        }
+        while(!map.containsKey(currentMonthGet)) {
+          currentMonthGet = currentMonthGet.minusDays(1);
+        }
+        list.add(currentMonthEnd.getYear() + "-"
+                + currentMonthEnd.getMonthValue() + ":" /*+ map.get
+        (currentMonthGet)*/);
+        listAmount.add(map.get(currentMonthGet));
+        currentDate = currentMonthEnd.plusDays(1);
+        //monthCount--;
+      }
+    } else if ((monthCount / 3) < 29) {
+      // split it to 3 month
+      // find the last working day of this month
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentMonthEnd = getQuarterEnd(currentDate);
+        LocalDate currentMonthGet = currentMonthEnd;
+        if(currentMonthEnd.isAfter(endDate)) {
+          currentMonthEnd = endDate;
+          currentMonthGet = endDate;
+        }
+        while(!map.containsKey(currentMonthGet)) {
+          currentMonthGet = currentMonthGet.minusDays(1);
+        }
+        list.add(currentDate.getYear() + "-" + currentDate.getMonthValue() + "to "
+                + currentMonthEnd.getYear() + currentMonthEnd.getMonthValue() +
+                ":" /*+ map
+        .get
+        (currentMonthGet)*/);
+        listAmount.add(map.get(currentMonthGet));
+        currentDate = currentMonthEnd.plusDays(1);
+        //monthCount = monthCount -3;
+      }
+    } else if (yearCount <= 30) {
+      //split it to year
+      //fins the last working day of this year
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentYearEnd = currentDate.withDayOfYear(currentDate.lengthOfYear());
+        LocalDate currentYearGet = currentYearEnd;
+        if(currentYearEnd.isAfter(endDate)) {
+          currentYearEnd = endDate;
+          currentYearGet = endDate;
+        }
+        while(!map.containsKey(currentYearGet)) {
+          currentYearGet = currentYearGet.minusDays(1);
+        }
+        list.add(currentDate.getYear() + ":" /*+ map.get(currentYearGet)*/);
+        listAmount.add(map.get(currentYearGet));
+        currentDate = currentYearEnd.plusDays(1);
+        //yearCount--;
+      }
+    } else {
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentYearEnd = currentDate.withDayOfYear(currentDate.lengthOfYear());
+        LocalDate currentYearGet = currentYearEnd;
+        if(currentYearEnd.isAfter(endDate)) {
+          currentYearEnd = endDate;
+          currentYearGet = endDate;
+        }
+        while(!map.containsKey(currentYearGet)) {
+          currentYearGet = currentYearGet.minusDays(1);
+        }
+        list.add(currentDate.getYear() + ":" /*+ map.get(currentYearGet)*/);
+        listAmount.add(map.get(currentYearGet));
+        currentDate = currentYearEnd.plusDays(1).plusYears(1);
+        //yearCount--;
+      }
+    }
+
+    //
+    Double maxAmount =  Collections.max(listAmount);
+    Double minAmount =  Collections.min(listAmount);
+    if (maxAmount.equals(0.0) && minAmount.equals(0.0)) {
+      scale = "all the performance are 0";
+      for(int i = 0; i< listAmount.size(); i++) {
+        listStar.add("");
+      }
+      return;
+    }
+    if(minAmount.equals(0.0)) {
+      minAmount = listAmount.get(0);
+      for(int i = 1; i<listAmount.size(); i++) {
+        if(listAmount.equals(0.0)) {
+          continue;
+        }
+        if(listAmount.get(i) < minAmount) {
+          minAmount = listAmount.get(i);
+        }
+      }
+    }
+
+    double more = (maxAmount-minAmount)/30;
+    double base = minAmount-more;
+    scale = "one asterisk is $" + more + "more than a base amount of $" + base;
+
+    for(int i = 0; i<listAmount.size(); i++) {
+      String star ="";
+      if(!listAmount.get(i).equals(0.0)) {
+        for(int j=0; j < (int) ((listAmount.get(i) - base) / more); j++) {
+          star = star + "*";
+        }
+      }
+      listStar.add(star);
+    }
+  }
+
+
+  private static LocalDate getQuarterEnd(LocalDate localDate) {
+    localDate = localDate.withDayOfMonth(localDate.lengthOfMonth());
+    long month = localDate.getMonth().getValue();
+    if (1 <= month && month <= 3) {
+      localDate = localDate.withMonth(3);
+      localDate = localDate.withDayOfMonth(localDate.lengthOfMonth());
+    } else if (4 <= month && month <= 6) {
+      localDate = localDate.withMonth(6);
+      localDate = localDate.withDayOfMonth(localDate.lengthOfMonth());
+    } else if (7 <= month && month <= 9) {
+      localDate = localDate.withMonth(9);
+      localDate = localDate.withDayOfMonth(localDate.lengthOfMonth());
+    } else {
+      localDate = localDate.withMonth(12);
+      localDate = localDate.withDayOfMonth(localDate.lengthOfMonth());
+    }
+    return localDate;
+  }
+
+  @Override
+  public View getView() {
+    return viewFactory.newPerformacePageView(portfolioName, startDate, endDate, list,
+            listStar, scale, errorMessage);
+  }
+
+  @Override
+  public PageController handleInput(String input) {
+    input = input.trim();
+    errorMessage = null;
+    portfolioName = portfolioService.getPortfolio().getName();
+
+    if (input.equals("back")) {
+      return controllerFactory.newMainPageController();
+    }
+    try {
+      String[] cmd = input.split(",");
+      if (cmd.length != 2) {
+        errorMessage = "Error Format!";
+        return this;
+      }
+      startDate = LocalDate.parse(cmd[0]);
+      endDate = LocalDate.parse(cmd[1]);
+
+      if(endDate.isBefore(startDate)) {
+        errorMessage = "Error: The start date cannot after the end date!";
+      }
+      map = portfolioService.getValues(startDate, endDate);
+
+      // how to know the start date and end date have value?
+      if(!map.containsKey(startDate)) {
+        errorMessage = "Error: Please choose input new timespan."
+                + "This start date maybe the holiday or weekend!";
+        startDate = null;
+        endDate = null;
+        map = new HashMap<>();
+        return this;
+      }
+    }catch (Exception e) {
+        errorMessage = "Error Date!";
+        startDate = null;
+        endDate = null;
+        map = new HashMap<>();
+        return this;
+    }
+    updateOutput(startDate,endDate);
+
+    /*List<Double> listAmount = new ArrayList<>();
+    long dayCount = ChronoUnit.DAYS.between(startDate,endDate) + 1;
+    long weekCount = ChronoUnit.WEEKS.between(startDate,endDate) + 1;
+    long monthCount = ChronoUnit.MONTHS.between(startDate,endDate) + 1 ;
+    long yearCount = ChronoUnit.YEARS.between(startDate,endDate) + 1;
+    LocalDate currentDate =  startDate;
+    if(dayCount <= 30) {
+      //output the list
+      while (!currentDate.isAfter(endDate)) {
+        if(map.containsKey(currentDate)) {
+          list.add(currentDate + ":");
+          listAmount.add(map.get(currentDate));
+          currentDate = currentDate.plusDays(1);
+        } else {
+          LocalDate currentGet = currentDate.minusDays(1);
+          while(!map.containsKey(currentGet)) {
+            currentGet = currentGet.minusDays(1);
+          }
+          list.add(currentDate + ":");
+          listAmount.add(map.get(currentGet));
+        }
+      }
+    } else if (weekCount <=30) {
+      //split it to weeks
+      // find the last working day of this week
+      while(!currentDate.isAfter(endDate)) {
+        LocalDate currentWeekEnd = currentDate.with(ChronoField.DAY_OF_WEEK, 7);
+        LocalDate currentWeekGet = currentDate.with(ChronoField.DAY_OF_WEEK, 5);
+        if (currentWeekEnd.isAfter(endDate)) {
+          currentWeekEnd = endDate;
+          currentWeekGet = endDate;
+        }
+        while(!map.containsKey(currentWeekGet)) {
+          currentWeekGet = currentWeekGet.minusDays(1);
+        }
+        list.add(currentDate + "to " + currentWeekEnd + ":" *//*+ map.get(currentWeekGet)*//*);
+        listAmount.add(map.get(currentWeekGet));
+        currentDate = currentWeekEnd.plusDays(1);
+      }
+    } else if (monthCount <= 30) {
+      //split it to month
+      // find the last working day of this month
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentMonthEnd = currentDate.withDayOfMonth(currentDate.lengthOfMonth());
+        LocalDate currentMonthGet = currentMonthEnd;
+        if(currentMonthEnd.isAfter(endDate)) {
+          currentMonthEnd = endDate;
+          currentMonthGet = endDate;
+        }
+        while(!map.containsKey(currentMonthGet)) {
+          currentMonthGet = currentMonthGet.minusDays(1);
+        }
+        list.add(currentMonthEnd.getYear() + "-"
+                + currentMonthEnd.getMonthValue() + ":" *//*+ map.get
+        (currentMonthGet)*//*);
+        listAmount.add(map.get(currentMonthGet));
+        currentDate = currentMonthEnd.plusDays(1);
+        //monthCount--;
+      }
+    } else if ((monthCount / 3) < 29) {
+      // split it to 3 month
+      // find the last working day of this month
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentMonthEnd = getQuarterEnd(currentDate);
+        LocalDate currentMonthGet = currentMonthEnd;
+        if(currentMonthEnd.isAfter(endDate)) {
+          currentMonthEnd = endDate;
+          currentMonthGet = endDate;
+        }
+        while(!map.containsKey(currentMonthGet)) {
+          currentMonthGet = currentMonthGet.minusDays(1);
+        }
+        list.add(currentDate.getYear() + "-" + currentDate.getMonthValue() + "to "
+                + currentMonthEnd.getYear() + currentMonthEnd.getMonthValue() +
+                ":" *//*+ map
+        .get
+        (currentMonthGet)*//*);
+        listAmount.add(map.get(currentMonthGet));
+        currentDate = currentMonthEnd.plusDays(1);
+        //monthCount = monthCount -3;
+      }
+    } else if (yearCount <= 30) {
+      //split it to year
+      //fins the last working day of this year
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentYearEnd = currentDate.withDayOfYear(currentDate.lengthOfYear());
+        LocalDate currentYearGet = currentYearEnd;
+        if(currentYearEnd.isAfter(endDate)) {
+          currentYearEnd = endDate;
+          currentYearGet = endDate;
+        }
+        while(!map.containsKey(currentYearGet)) {
+          currentYearGet = currentYearGet.minusDays(1);
+        }
+        list.add(currentDate.getYear() + ":" *//*+ map.get(currentYearGet)*//*);
+        listAmount.add(map.get(currentYearGet));
+        currentDate = currentYearEnd.plusDays(1);
+        //yearCount--;
+      }
+    } else {
+      while (!currentDate.isAfter(endDate)) {
+        LocalDate currentYearEnd = currentDate.withDayOfYear(currentDate.lengthOfYear());
+        LocalDate currentYearGet = currentYearEnd;
+        if(currentYearEnd.isAfter(endDate)) {
+          currentYearEnd = endDate;
+          currentYearGet = endDate;
+        }
+        while(!map.containsKey(currentYearGet)) {
+          currentYearGet = currentYearGet.minusDays(1);
+        }
+        list.add(currentDate.getYear() + ":" *//*+ map.get(currentYearGet)*//*);
+        listAmount.add(map.get(currentYearGet));
+        currentDate = currentYearEnd.plusDays(1).plusYears(1);
+        //yearCount--;
+      }
+    }
+
+    //
+    Double maxAmount =  Collections.max(listAmount);
+    Double minAmount =  Collections.min(listAmount);
+    if (maxAmount.equals(0.0) && minAmount.equals(0.0)) {
+      scale = "all the performance are 0";
+      for(int i = 0; i< listAmount.size(); i++) {
+        listStar.add("");
+      }
+      return this;
+    }
+    if(minAmount.equals(0.0)) {
+      minAmount = listAmount.get(0);
+      for(int i = 1; i<listAmount.size(); i++) {
+        if(listAmount.equals(0.0)) {
+          continue;
+        }
+        if(listAmount.get(i) < minAmount) {
+          minAmount = listAmount.get(i);
+        }
+      }
+    }
+
+    double more = (maxAmount-minAmount)/30;
+    double base = minAmount-more;
+    scale = "one asterisk is $" + more + "more than a base amount of $" + base;
+
+    for(int i = 0; i<listAmount.size(); i++) {
+      String star ="";
+      if(!listAmount.get(i).equals(0.0)) {
+        for(int j=0; j < (int) ((listAmount.get(i) - base) / more); j++) {
+          star = star + "*";
+        }
+      }
+      listStar.add(star);
+    }*/
+    return this;
+  }
+
+
+}
---
+++ neu-fall22-pdp-hw4/src/portfolio/views/impl/PerformancePageView.java
@@ -0,0 +1,74 @@
+package portfolio.views.impl;
+
+import java.time.LocalDate;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+import portfolio.views.ViewAbs;
+
+public class PerformancePageView extends ViewAbs {
+
+  private String errorMessage;
+
+  private String portfolioName;
+
+  private LocalDate startDate;
+  private LocalDate endDate;
+
+  private List<String> listStar = new ArrayList<>();
+  private List<String> list = new ArrayList<>();
+  private String scale;
+
+
+  public PerformancePageView(String portfolioName,
+                             LocalDate startDate,
+                             LocalDate endDate,
+                             List<String> list,
+                             List<String> listStar,
+                             String scale,
+                             String errorMessage) {
+    this.portfolioName = portfolioName;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.list = list;
+    this.listStar = listStar;
+    this.scale = scale;
+    this.errorMessage = errorMessage;
+  }
+
+  @Override
+  public void render() {
+    clearConsole();
+    printStream.println("*************This is the interface for Performance**************");
+    if (errorMessage != null) {
+      printStream.println("---------------------ERROR--------------------------------");
+      printStream.println("! Error message: " + errorMessage);
+      printStream.println("----------------------------------------------------------");
+    }
+    printStream.println("*********************************************************");
+    printStream.println("!!! If you enter back, you will back to the main menu.");
+    printStream.println("*********************************************************");
+    printStream.println("Please enter the scale of the timespan " +
+            "that you want to performance.");
+    printStream.println("--EX.2020-10-11,2022-10-11");
+    printStream.println("--The format of date needs to be 2022-10-11");
+    printStream.println("--There is a comma between the two date and no space between them.");
+    printStream.print("input > ");
+    //printStream.println("--Other input except back will be error.");
+
+    if (startDate != null && endDate != null) {
+      printStream.println("Performance of portfolio" + portfolioName
+              + "from" + startDate + "to" + endDate);
+      for (int i = 0; i < list.size(); i++) {
+        printStream.println(list.get(i) + listStar.get(i));
+      }
+      printStream.println("scale: $" + scale);
+    }
+
+  }
+}
+
+
